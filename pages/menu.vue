1d928858acf237b6ecfefdb92b268fa03e4a92ef
Zulfafik <zulfafikeiazizah@gmail.com>
2022-03-17 19:26:29 +0700

    coba

---
+++ pages/menu.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <h1>Welcome !</h1>
+    <div class="card">
+      <p>Sign in with</p>
+      <div class="buttonLogo">
+        <a href="/login"><img :src="require('~/assets/images/logo.png')" /></a>
+      </div>
+    </div>
+  </div>
+</template>
+<script></script>
+<style>
+h1 {
+  color: aliceblue;
+}
+.container {
+  margin: 0;
+  padding: 50px;
+  font-family: "Verdana", sans-serif;
+  text-align: center;
+  background-color: brown;
+}
+.card {
+  width: 440px;
+  padding: 20px;
+  background-color: rgb(255, 255, 255);
+  border-radius: 15px;
+}
+.buttonLogo {
+  width: 200px;
+  box-shadow: 2px 2px 8px;
+  border-radius: 15px;
+}
+</style>
---
+++ pages/signup/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <h1>Hello this is Sign Up page</h1>
+</template>
+<script>
+export default {};
+</script>
